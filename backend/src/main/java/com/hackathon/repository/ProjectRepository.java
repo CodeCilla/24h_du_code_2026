c25bf286c15fa3e98400cedadd319118caa96921
CodeCilla <priscilla.renault72460@gmail.com>
2026-03-21 11:34:36 +0100

    feat: delete project

--- backend/src/main/java/com/hackathon/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hackathon.repository;
-
-import com.hackathon.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
